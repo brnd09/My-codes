d483c479cee15b555d8814bd70c3a5df22ae5e43
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2023-05-29 00:12:44 -0300

    Delete GitHub/My-codes directory

--- GitHub/My-codes/Python/aula-04/aula-04.6.py
+++
@@ -1,4 +0,0 @@
-# DESAFIO 009 FAÇA UM PROGRAMA QUE LEIA UM NÚMERO QUALQUER E MOSTRE NA TELA SUA TABUADA 
-
-n1 =int(input('Escreva um número para que mostre a sua tabuada: '))
-print('{} X 1 = {}\n{} X 2 = {}\n{} X 3 = {}\n{} X 4 = {} \n{} X 5 = {}\n{} X 6 = {}\n{} X 7 = {}\n{} X 8 = {}\n{} X 9 = {} \n{} X 10 = {}'.format(n1, n1*1,n1, n1*2,n1, n1*3, n1, n1*4, n1, n1*5, n1, n1*6, n1,  n1*7, n1,  n1*8, n1, n1*9, n1, n1*10))
