ca24225f8dfaa52c07216c1440b237e74d983e1e
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2023-05-29 00:11:33 -0300

    Delete GitHub/My-codes/Python/scripts-python-curso directory

--- GitHub/My-codes/Python/scripts-python-curso/aula-03/aula-03.1.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Digite um número'))
-n2 = int(input('Digite outro número'))
-s = n1+n2
-print ('a soma entre', n1, 'e', n2, 'vale', s)
--- GitHub/My-codes/Python/scripts-python-curso/aula-05/aula-05.1.py
+++
@@ -1,7 +0,0 @@
-import math
-# DESAFIO 016 - CRIE UM PROGRAMA QUE LEIA UM NÚMERO REAL QUALQUER PELO TECLADO E MOSTRE NA TELA A SUA PORÇÃO INTEIRA
-# EXEMPLO: DIGITE UM NÚMERO: 6.127 / O NÚMERO 6.127 TEM A PARTE INTEIRA 6
-
-n1 = float(input('Escreva qualquer número, para mostrar sua porção inteira: '))
-inter = (math.trunc(n1))
-print(f'O número {n1} tem a parte inteira {inter}')
